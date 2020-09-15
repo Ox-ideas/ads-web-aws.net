4b6ec4f0eaaccfeaf19c097d12fc84fcaec6bbd6
sunnygb <sunnygb.8@gmail.com>
2020-09-15 19:44:01 +0800

    Prepare data models for user registration, confirmation and login

---
+++ Ads.Web/Models/Accounts/SignupModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ads.Web.Models.Accounts
+{
+    public class SignupModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name="Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [StringLength(6,ErrorMessage ="Password must be at least six characters long.")]
+        [Display(Name ="Password")]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage ="")]
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        public string GivenName { get; set; }
+
+        [Required]
+        public string FamilyName { get; set; }
+
+        [Required]
+        public string Phone { get; set; }
+    }
+}
